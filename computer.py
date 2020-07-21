ea6dad21ca165f0e45544228cb35acea19d348c4
heartlessglint <58626018+heartlessglint@users.noreply.github.com>
2020-07-21 13:23:36 +0100

    Update computer.py

--- computer.py
+++ computer.py
@@ -1,16 +1,13 @@
-class Computer(Deck.Deck):
+from DeckOfCards import Deck
+
+class Computer(Deck):
   def __init__(self, number):
     self.number = number
 
 
   def start(self, deck):
     hand = []
-    card = self.draw(deck)
-    hand.append(card)
+    while len(hand) < 2:
+      card = self.draw(deck)
+      hand.append(card)
     return hand
-
-computer = Computer("1")
-
-test = computer.start(shuffled)
-
-print(test)
