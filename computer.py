e7be0155214c8b44c7ee7adf4a2b2a90f3b20198
heartlessglint <58626018+heartlessglint@users.noreply.github.com>
2020-07-21 13:43:38 +0100

    Update computer.py

--- computer.py
+++ computer.py
@@ -11,3 +11,23 @@ class Computer(Deck):
       card = self.draw(deck)
       hand.append(card)
     return hand
+
+  def drawCard(self, total):
+    if total == 21:
+      print("I have 21, I will not draw")
+      return False
+    elif total > 21:
+      print("I have lost, what a shame")
+      return False
+    elif 19 < total < 21:
+      print("I am too close, I will not go again")
+      return False
+    elif total == 18 and random.randint(1,101) > 95:
+      print("Very risky, but I'll take one")
+      return True
+    elif total == 17 and random.randint(1,101) > 80:
+      print("I'll go for the risk, give me a card")
+      return True
+    elif total == 16 and random.randint(1,101) > 50:
+      print("Risky, but I'll take another")
+      return True
